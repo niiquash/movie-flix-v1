a65764d0e42c395385b94a37457027443df5cb7d
niiquash <qua16006@byui.edu>
2023-06-12 16:58:08 -0600

    bug fix on hero poster image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Hero from '../hero/Hero';
+import HeroPage from '../hero/HeroPage';
 
 const Home = ({movies}) => {
   return (
-    <Hero movies={movies} />
+    <HeroPage movies={movies} />
   )
 }
 
