d17c1674d26866237cf0762b133dcaad29cbd9e8
niiquash <qua16006@byui.edu>
2023-06-13 22:40:12 -0600

    bootstrap bug fix

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import HeroPage from '../hero/HeroPage';
+import Header from '../header/Header';
 
 const Home = ({movies}) => {
   return (
-    <HeroPage movies={movies} />
+    <>
+      <Header />
+      <HeroPage movies={movies} />
+    </>
   )
 }
 
