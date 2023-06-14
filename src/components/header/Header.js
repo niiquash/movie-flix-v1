dd6fdc18230383f74bde15bf452200eb9883d4c0
niiquash <qua16006@byui.edu>
2023-06-13 22:41:41 -0600

    bootstrap bug fix

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import { faVideoSlash } from '@fortawesome/free-solid-svg-icons';
 import { Button } from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import Navbar from "react-bootstrap";
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
